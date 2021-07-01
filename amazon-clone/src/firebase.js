a5a39a9e5366c9c8b70075b5a7ed7af2a28ec9f0
szilard-molnar <mszilard86@gmail.com>
2021-06-30 23:30:42 -0400

    firebase.js added

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB191lvgcusiTx5lFz3y7ohmFb3ZMw-sqU",
+    authDomain: "clone-6ae1d.firebaseapp.com",
+    projectId: "clone-6ae1d",
+    storageBucket: "clone-6ae1d.appspot.com",
+    messagingSenderId: "290987952511",
+    appId: "1:290987952511:web:c09671addea90be007724a"
+};
