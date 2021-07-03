434005ef9c53105deb4fa7fd5c09d9666c7f01b3
szilard-molnar <mszilard86@gmail.com>
2021-07-03 13:44:57 -0400

    home page product components are filled through props

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -14,22 +14,53 @@ function Home() {
 
                 <div className="home__row">
                     <Product 
-                        title='The lean startup'
+                        id="12321341"
+                        title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
                         price={29.99}
                         image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
                         rating={5}
                     />
-                    <Product />
+                    <Product 
+                        id="49538094"
+                        title="Kenwood kMix Stand Mixer fro Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                        price={239.00}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/61Jhk2iYwHL._AC_UY218_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product 
+                        id="4903850"
+                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                        price={199.99}
+                        rating={3}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81IyIIgqQPL.__AC_SY300_SX300_QL70_FMwebp_.jpg"
+                    />
+                    <Product 
+                        id="23445930"
+                        title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                        price={98.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/714Rq4k05UL._AC_SL1000_.jpg"
+                    />
+                    <Product 
+                        id="325435345"
+                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) Silver (4th Generation)"
+                        price={598.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81FH2j7EnJL._AC_SL1500_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
-                    <Product />
+                    <Product 
+                        id="90729332"
+                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+                        price={1094.98}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/91-hWcNu2yL._AC_SL1500_.jpg"
+                    />
                 </div>
             </div>
         </div>
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './Product.css';
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>&#11088;</p> {/* Render Star */}
-                    <p>&#11088;</p>
-                    <p>&#11088;</p>
+                    {Array(rating).fill().map((_, i) => (
+                        <p>&#11088;</p> //Render stars
+                    ))}
+                    
                 </div>
             </div>
             <img 
                 className=""
-                src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+                src={image}
                 alt=""
             />
 
