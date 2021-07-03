155cd9c5278852a2a68b6e3999e79e48ae72e02b
szilard-molnar <mszilard86@gmail.com>
2021-07-03 17:39:36 -0400

    subtotal component added with currencyFormat

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
     return (
@@ -11,11 +12,16 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your Shopping Basket
                     </h2>
+
+                    {/* BasketItem */}
+                    {/* BasketItem */}
+                    {/* BasketItem */}
+
                 </div>
             </div>
 
             <div className="checkout__right">
-
+                <Subtotal />
             </div>
         </div>
     )
