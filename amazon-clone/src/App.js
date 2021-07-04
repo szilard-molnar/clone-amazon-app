0d9fd9bab83842e9175ac6b4e7a1f0056c296cb3
szilard-molnar <mszilard86@gmail.com>
2021-07-04 12:12:52 -0400

    logipage styling added

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -4,17 +4,24 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
+
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
