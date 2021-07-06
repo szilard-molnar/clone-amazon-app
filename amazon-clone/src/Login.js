9b7cbefa118532a72e70be065b4825a5a10242bc
szilard-molnar <mszilard86@gmail.com>
2021-07-05 21:04:48 -0400

    user authentication works with firebase

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from './firebase';
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+        // Firebase login
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/');
+            })
+            .catch(error => alert(error.message));
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+
+        // Firebase register
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                //successfully created a new user.
+                console.log(auth);
+                if(auth) {
+                    history.push('/');
+                }
+            })
+            .catch(error => alert(error.message));
+    }
+
     return (
         <div className="login">
             <Link to='/'>
@@ -17,19 +50,19 @@ function Login() {
                 
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text"/>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
-                    <input type="password"/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
 
-                    <button className="login__signInButton">Sign In</button>
+                    <button className="login__signInButton" onClick={signIn} type="submit">Sign In</button>
                 </form>
 
                 <p>
                     By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className="login__registerButton">Create your Amazon Account</button>
+                <button className="login__registerButton" onClick={register}>Create your Amazon Account</button>
             </div>
         </div>
     )
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB191lvgcusiTx5lFz3y7ohmFb3ZMw-sqU",
     authDomain: "clone-6ae1d.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "290987952511",
     appId: "1:290987952511:web:c09671addea90be007724a"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
