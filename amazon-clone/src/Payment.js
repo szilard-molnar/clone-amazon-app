6110a764dbedebe95cfca7870d26ed6fda6e0860
szilard-molnar <mszilard86@gmail.com>
2021-07-15 21:23:00 -0400

    database connection created, adding data to database

--- amazon-clone/src/Payment.js
+++ amazon-clone/src/Payment.js
@@ -7,6 +7,7 @@ import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import CurrencyFormat from 'react-currency-format';
 import { getBasketTotal } from './reducer';
 import axios from './axios';
+import { db } from './firebase';
 
 function Payment() {
     const [{basket, user}, dispatch] = useStateValue();
@@ -52,6 +53,17 @@ function Payment() {
             }
         }).then(({ paymentIntent }) => {
             //paymentIntend == payment confirmation
+
+            db
+                .collection('users')
+                .doc(user?.id)
+                .collection('orders')
+                .doc(paymentIntent.id)
+                .set({
+                    basket: basket,
+                    amount: paymentIntent.amount,
+                    created: paymentIntent.created
+                })
 
             setSucceeded(true);
             setError(null);
