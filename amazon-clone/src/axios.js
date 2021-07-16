5e95ae7ddfbfa4ac5c60f18747645b902680cf13
szilard-molnar <mszilard86@gmail.com>
2021-07-15 23:44:31 -0400

    deployed app works, this is after build

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-6ae1d/us-central1/api' // THE API URL (Cloud Function URL)
+    baseURL: 'https://us-central1-clone-6ae1d.cloudfunctions.net/api'
+    // 'http://localhost:5001/clone-6ae1d/us-central1/api' THE API URL (Cloud Function URL local for debugging)
 });
-
 export default instance;
