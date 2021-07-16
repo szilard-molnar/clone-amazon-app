39a8dc443104686867ae6d922db9aa5e79196822
szilard-molnar <mszilard86@gmail.com>
2021-07-15 23:05:39 -0400

    orders page is calling from database

--- amazon-clone/src/Order.js
+++ amazon-clone/src/Order.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Order.css';
 import moment from 'moment';
 import CheckoutProduct from './CheckoutProduct';
+import CurrencyFormat from 'react-currency-format';
 
 function Order({ order }) {
     return (
@@ -19,8 +20,19 @@ function Order({ order }) {
                     image={item.image}
                     price={item.price}
                     rating={item.rating}
+                    hideButton
                 />
             ))}
+            <CurrencyFormat
+                renderText={(value) => (
+                    <h3 className="order__total">Order Total: {value}</h3>
+                )}
+                decimalScale={2}
+                value={order.data.amount / 100}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
         </div>
     )
 }
